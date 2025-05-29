7101ff9c68c14400f8e5467177e189326f6eb99a
Sebastian Malasquez <sebasmalasquez@gmail.com>
2025-05-28 22:16:15 -0500

    Limpio

--- react/src/Portada.js
+++ react/src/Portada.js
@@ -6,20 +6,7 @@ function App() {
     <div className="main">
       
       <Navegador />
-      <div>
-      <div className="bloque">
-          <h1 className='tit-1'>Todo saludable</h1>
-          <h4 className="tit-2">Conoce toda nuestra línea de repostería fina basada en una opción saludable: los endulzantes naturaless
-            ¡Aprendamos a comer sin culpa!</h4>
-          <button className='btn-portada shadow-sm'>Ver catálogo</button>
-        </div>
-        <div>
-          
-      </div>
-    
-    </div> <div className='bloque-2'>
-      <button>sef</button>
-    </div></div>
+    </div>
    
   );
 }
